c9287a640df6dd90991ca7abac67359186469950
lazyfuhrer <iambiswarghya@gmail.com>
2024-01-10 23:33:56 +0530

    feat: added test run

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use bytes::Bytes;
+use hex::{self};
+use light_evm::{
+    execution_context::ExecutionContext, instruction::Instruction, opcodes::Opcodes,
+};
+use std::env::args;
+
 fn main() {
-    println!("Hello, world!");
+    Opcodes::register_instructions();
+
+    let hex_str = args().nth(1).expect("No bytecode passed");
+    let byte_array = hex::decode(&hex_str).expect("Invalid hex string");
+    let code = Bytes::from(byte_array);
+    let mut context = ExecutionContext::new(code);
+
+    while !context.stopped {
+        let pc_before = context.pc.clone();
+        let instruction = Instruction::decode_opcode(&mut context).unwrap();
+        instruction.executor.execute(&mut context);
+
+        println!("{:?} @ pc={}", instruction.name, pc_before);
+        println!("Stack: {:?}", context.stack.stack);
+        println!("Memory: {:?}", context.memory.memory);
+        println!("---------");
+    }
+
+    println!("{}", format!("Output : 0x{:x}", context.returndata));
 }
